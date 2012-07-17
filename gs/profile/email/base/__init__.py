6fb4c8e10d524c6601d21a29179c6e74c28ed37d
Michael JasonSmith <mpj17@onlinegroups.net>
2012-07-17 10:11:38 +1000

    Importing the EmailUser class into the __init__ for the module.

--- gs/profile/email/base/__init__.py
+++ gs/profile/email/base/__init__.py
@@ -5,5 +5,5 @@ from AccessControl import allow_class
 eu_security = ModuleSecurityInfo('gs.profile.email.base.emailuser')
 eu_security.declarePublic('EmailUser')
 
-from gs.profile.email.base.emailuser import EmailUserFromUser
+from gs.profile.email.base.emailuser import EmailUserFromUser, EmailUser
 allow_class(EmailUserFromUser)
