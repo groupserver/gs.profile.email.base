33d92fe33025a90dccad2f14ef35bdda9a6dc71f
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-23 11:15:15 -0800

    Adding the NewEmailAddress and EmailAddressExists to the general API

--- gs/profile/email/base/__init__.py
+++ gs/profile/email/base/__init__.py
@@ -1,9 +1,13 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
 
 eu_security = ModuleSecurityInfo('gs.profile.email.base.emailuser')
 eu_security.declarePublic('EmailUser')
 
-from gs.profile.email.base.emailuser import EmailUserFromUser, EmailUser
+#lint:disable
+from .emailuser import EmailUserFromUser, EmailUser
+from .emailaddress import NewEmailAddress, EmailAddressExists
+#lint:enable
 allow_class(EmailUserFromUser)
