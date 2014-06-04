729c07789487bfa6fb1c5dc818315b078aa09bad
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 09:30:45 -0700

    PEP-8, Unicode and Lazy updates to the vocab

--- gs/profile/email/base/vocabularies.py
+++ gs/profile/email/base/vocabularies.py
@@ -1,12 +1,28 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from zope.interface import implements, providedBy
 from zope.interface.common.mapping import IEnumerableMapping
-from zope.schema.interfaces import IVocabulary, \
-  IVocabularyTokenized, ITitledTokenizedTerm
-from zope.schema.vocabulary import SimpleTerm 
+from zope.schema.interfaces import IVocabulary, IVocabularyTokenized, \
+    ITitledTokenizedTerm
+from zope.schema.vocabulary import SimpleTerm
 from Products.CustomUserFolder.interfaces import IGSUserInfo
-from emailuser import EmailUser
+from .emailuser import EmailUser
+
 
 class EmailAddressesForUserInfo(object):
     implements(IVocabulary, IVocabularyTokenized)
@@ -17,17 +33,15 @@ class EmailAddressesForUserInfo(object):
         self.emailUser = EmailUser(userInfo.user, userInfo)
         self._addresses = None
 
-    @property
+    @Lazy
     def addresses(self):
-        if self._addresses == None:
-            self._addresses = \
-              self.emailUser.get_verified_addresses()
-        assert type(self._addresses) == list
-        return self._addresses
-        
+        retval = self.emailUser.get_verified_addresses()
+        assert type(retval) == list
+        return retval
+
     def __iter__(self):
         """See zope.schema.interfaces.IIterableVocabulary"""
-        retval = [SimpleTerm(a, a, a) 
+        retval = [SimpleTerm(a, a, a)
                   for a in self.addresses]
         for term in retval:
             assert term
@@ -52,17 +66,19 @@ class EmailAddressesForUserInfo(object):
     def getTerm(self, value):
         """See zope.schema.interfaces.IBaseVocabulary"""
         return self.getTermByToken(value)
-        
+
     def getTermByToken(self, token):
         """See zope.schema.interfaces.IVocabularyTokenized"""
         for a in self.addresses:
             if a == token:
-                retval = SimpleTerm(a, a, a) 
+                retval = SimpleTerm(a, a, a)
                 assert retval
                 assert ITitledTokenizedTerm in providedBy(retval)
                 #assert retval.token == retval.value
                 return retval
-        raise LookupError, token
+        m = 'Token "{0}" not found'.format(token)
+        raise LookupError(m)
+
 
 class EmailAddressesForLoggedInUser(EmailAddressesForUserInfo):
     """ Similar to EmailAddressesForUser, but makes the assumption
@@ -74,10 +90,10 @@ class EmailAddressesForLoggedInUser(EmailAddressesForUserInfo):
         self.emailUser = EmailUser(context, userInfo)
         self._addresses = None
 
+
 class EmailAddressesForUser(EmailAddressesForUserInfo):
     def __init__(self, user):
         userInfo = IGSUserInfo(user)
         self.context = user
         self.emailUser = EmailUser(userInfo.user, userInfo)
         self._addresses = None
-
