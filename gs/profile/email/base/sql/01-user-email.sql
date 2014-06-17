af3b0b5ebb4762e20fb034db52553525b8c8aff2
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-17 16:43:47 -0700

    Tidying up the SQL some.

--- gs/profile/email/base/sql/01-user-email.sql
+++ gs/profile/email/base/sql/01-user-email.sql
@@ -1,18 +1,19 @@
 SET CLIENT_ENCODING = 'UTF8';
 SET CLIENT_MIN_MESSAGES = WARNING;
 
-CREATE TABLE USER_EMAIL (
-    USER_ID         TEXT                      NOT NULL,
-    EMAIL           TEXT                      UNIQUE NOT NULL,
-    IS_PREFERRED    BOOLEAN                   NOT NULL DEFAULT 'false',
-    VERIFIED_DATE   TIMESTAMP WITH TIME ZONE  DEFAULT NULL
+CREATE TABLE user_email (
+    user_id         TEXT                      NOT NULL,
+    email           TEXT                      UNIQUE NOT NULL,
+    is_preferred    BOOLEAN                   NOT NULL DEFAULT 'false',
+    verified_date   TIMESTAMP WITH TIME ZONE  DEFAULT NULL
 );
 
 -- The combination of user_id and email is unique within the system
 CREATE UNIQUE INDEX USER_ID_EMAIL_PKEY ON USER_EMAIL
        USING BTREE (user_id, email);
 
--- Email is unique within the system
+-- The **lowered** form of the email-address is unique within the
+-- system, and needs to be accessed quickly.
 CREATE UNIQUE INDEX USER_EMAIL_EMAIL_LOWER_IDX ON USER_EMAIL
        USING BTREE (lower(email));
 
