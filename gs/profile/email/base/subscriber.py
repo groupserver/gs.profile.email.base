50c48a8846b07c9880fbb91ffb4dfb21486cb549
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-19 14:06:31 -0700

    Adding a Remove subscriber

---
+++ gs/profile/email/base/subscriber.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from logging import getLogger
+log = getLogger('gs.profile.email.base.subscriber')
+from .interfaces import IGSEmailUser
+
+
+def remove_email_data(user, event):
+    log.info('Removing addresses for {0}'.user.getId())
+    emailUser = IGSEmailUser(user)
+    for address in emailUser.get_addresses():
+        emailUser.remove_address(address)
