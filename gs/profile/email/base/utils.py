fa99f05d246c73c8910b6cfadc7eabf3dd3a2705
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-19 14:06:15 -0700

    Adding some Python 3 support

--- gs/profile/email/base/utils.py
+++ gs/profile/email/base/utils.py
@@ -13,16 +13,21 @@
 #
 ##############################################################################
 from __future__ import unicode_literals
+import sys
+if (sys.version_info < (3, )):
+    stringtype = basestring
+else:
+    stringtype = str
 from email.utils import parseaddr
 
 
 def sanitise_address(emailAddress):
-    if not isinstance(emailAddress, basestring):
+    if not isinstance(emailAddress, stringtype):
         m = '{0} is not a string'.format(type(emailAddress))
         raise TypeError(m)
 
     addrName, retval = parseaddr(emailAddress)
     retval = retval.strip(',.!?; \t\n\r<>(){}[]')
 
-    assert isinstance(retval, basestring)
+    assert isinstance(retval, stringtype)
     return retval
